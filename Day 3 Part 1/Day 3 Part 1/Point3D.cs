e693a8345fd48ce7a678337f6fa54b221503d819
Stefan Pahlplatz <stefan314@live.nl>
2016-12-03 12:25:29 +0100

    Day 3 Part 1

---
+++ Day 3 Part 1/Day 3 Part 1/Point3D.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3_Part_1
+{
+    class Point3D
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Z { get; private set; }
+
+        public Point3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
---
+++ Day 3 Part 1/Day 3 Part 1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day_3_Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Store input in 3D point
+            List<Point3D> triangles = new List<Point3D>();
+            
+            // Temporary string to store current line
+            string[] lines = File.ReadAllLines("input.txt");
+
+            // Loop through all lines
+            for (int i = 0; i < lines.Length; i++)
+            {
+                // Split the line on whitespaces
+                string[] split = lines[i].Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(s => s.Trim())
+                    .ToArray();
+
+                // Store splitted string in int
+                int x = int.Parse(split[0]);
+                int y = int.Parse(split[1]);
+                int z = int.Parse(split[2]);
+
+                // Check if the triangle is legit
+                if (((x + y) > z) &&
+                    ((x + z) > y) &&
+                    ((y + z) > x))
+                {
+                    // Add splitted line to list
+                    triangles.Add(new Point3D(int.Parse(split[0]), int.Parse(split[1]), int.Parse(split[2])));
+                }
+            }
+
+            // Output
+            Console.WriteLine("Amount of possible triangles: {0}", triangles.Count);
+
+            // Keep the console open
+            Console.Read();
+        }
+    }
+}
