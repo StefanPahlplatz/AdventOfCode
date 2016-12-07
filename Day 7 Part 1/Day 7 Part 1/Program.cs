a2d1bd02a40456f72e85bf0f5fc6b82b130a36e2
Stefan Pahlplatz <stefan314@live.nl>
2016-12-07 22:00:05 +0100

    Day 7 Part 1
    finally, look me the most time out of all of them so far.

--- Day 7 Part 1/Day 7 Part 1/Program.cs
+++ Day 7 Part 1/Day 7 Part 1/Program.cs
@@ -12,62 +12,94 @@ namespace Day_7_Part_1
     {
         static void Main(string[] args)
         {
-            //string[] alllines = File.ReadAllLines("input.txt");       // Input file
-            //string[] alllines = { "wysextplwqpvipxdv[srzvtwbfzqtspxnethm]syqbzgtboxxzpwr[kljvjjkjyojzrstfgrw]obdhcczonzvbfby[svotajtpttohxsh]cooktbyumlpxostt" };
-            string[] alllines = { "aa[xyyx]ecce[lliiiill]dd" };               
+            string[] alllines = File.ReadAllLines("input.txt");
 
-            for (int i = 0; i < alllines.Length; i++)                   // Loop through all lines
+            int possible = 0;
+            bool isInsideBrackets = false;
+
+            for (int i = 0; i < alllines.Length; i++)
             {
-                bool isValid = true;
-                string line = alllines[i];                              // Store the current line in a string
-                List<string> bracketpart = new List<string>();          // Store all strings in the brackets
-                List<string> outsidebrackets = new List<string>();
-                int amountOfBracketPairs = 0;                           // Integer to keep track of how many pairs of brackets we've had
+                bool valid = true;
+                bool hasValidMirror = false;
 
-                for (int j = 0; j < line.Length; j++)                   // Loop through the whole line and extract parts in []
+                int indexIn = -1;
+                int indexOut = 0;
+                List<string> inside = new List<string>();
+                List<string> outside = new List<string>();
+                outside.Add("");
+
+                // Loop through current string
+                for (int j = 0; j < alllines[i].Length; j++)
                 {
-
-                    if (line[j] == '[')                                 // If the char is the start of a hypernet sequence
+                    // Start of bracket
+                    if (alllines[i][j] == '[')
                     {
-                        int start = j;
-                        int end;
-                        bracketpart.Add("");                            // Reserve new space in the list
-                        j++;                                            // Increment j to remove the first [
+                        isInsideBrackets = true;
+                        inside.Add("");
+                        indexIn++;
+                    }
+                    // End of bracket
+                    else if (alllines[i][j] == ']')
+                    {
+                        isInsideBrackets = false;
+                        outside.Add("");
+                        indexOut++;
+                    }
 
-                        while (line[j] != ']')                          // While we are not at the closing bracket
+                    // If we are outside brackets
+                    if (!isInsideBrackets && alllines[i][j] != '[' && alllines[i][j] != ']')
+                    {
+                        outside[indexOut] += alllines[i][j];
+                    }
+                    // If we are inside brackets
+                    else if (isInsideBrackets && alllines[i][j] != '[' && alllines[i][j] != ']')
+                    {
+                        inside[indexIn] += alllines[i][j];
+                    }
+                }
+                
+                // Outside parts
+                foreach (string item in outside)
+                {
+                    if (item.Distinct().Count() != 1) 
+                    {
+                        if (hasMirror(item))
                         {
-                            bracketpart[amountOfBracketPairs] += line[j];   // Add the character to the string
-                            j++;                                        // Next character
+                            hasValidMirror = true;
                         }
-                        end = j + 1;                                    // Position that the brackets stop
-                        line = line.Remove(start, end - start);         // Remove part of the string between brackets
-                        amountOfBracketPairs++;                         // Increment list index
-                        j = 0;                                          // Reset the counter so we loop through the whole string again
-                    }
+                    }                  
                 }
 
-                Console.WriteLine("Parts in brackets: ");
-                foreach (string item in bracketpart)
+                // Inside part
+                foreach (string item in inside)
                 {
-                    string first = item.Substring(0, item.Length / 2);
-                    if (first == Reverse(first) && isValid)
+                    if (item.Distinct().Count() != 1)
                     {
-                        isValid = true;
-                    }
-                    else
-                    {
-                        isValid = false;
-                    }
-                    Console.WriteLine("\t" + item);
+                        if(hasMirror(item))
+                        {
+                            valid = false;
+                        }
+                    } 
                 }
 
-                Console.WriteLine("\nRest of the string");
-                foreach (string item in outsidebrackets)
+                if (valid && hasValidMirror)
                 {
-                    Console.WriteLine("\t" + item);
+                    possible++;
                 }
-                Console.Read();
             }
+            Console.WriteLine("Amount of possibilities: " + possible);
+            Console.Read();
+        }
+
+        public static bool hasMirror(string input)
+        {
+            for (int i = 0; i < input.Length - 3; i++)
+            {
+                if (input[i] == input[i + 3] && input[i + 1] == input[i + 2] && input[i] != input[i + 1])
+                    return true;
+            }
+
+            return false;
         }
 
         public static string Reverse(string s)
