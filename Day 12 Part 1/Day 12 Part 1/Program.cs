d32c80f6a911b86add6e3a94306ddbce3164d2d2
Stefan Pahlplatz <stefan314@live.nl>
2016-12-26 18:02:17 +0100

    Day 12 Part 1 - Unfinished

--- Day 12 Part 1/Day 12 Part 1/Program.cs
+++ Day 12 Part 1/Day 12 Part 1/Program.cs
@@ -1,95 +1,134 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Day_12_Part_1
 {
     class Program
     {
-        private static int[] registers;
-
+        public static List<string> instructions = new List<string>();
+        public static int[] registers = new int[4];
         static void Main(string[] args)
         {
-            registers = new int[4];
+            string[] input = File.ReadAllLines(@"input.txt");
 
-            string[] instructions = File.ReadAllLines("input.txt");
+            foreach (string line in input)
+                instructions.Add(line);
 
-            for (int i = 0; i < instructions.Length; i++)
-            {
-                string[] matches = Regex.Split(instructions[i], @"\s");
+            // Part 1
+            calc();
+            Console.WriteLine("Part1: Register a = {0}", registers[0]);
 
-                if (matches[0] == "cpy")
-                {
-                    int copyvalue;
-                    int registerToCopyTo;
-
-                    // Assign the copyvalue
-                    if (Regex.IsMatch(matches[1], "[a-z]"))
-                        copyvalue = registers[GetIntFromString(matches[1])];
-                    else
-                        copyvalue = int.Parse(matches[1]);
-
-                    // Assign the registerToCopy
-                    registerToCopyTo = GetIntFromString(matches[2]);
-
-                    registers[registerToCopyTo] = copyvalue;
-                }
-                else if (matches[0][2] == 'c')
-                {
-                    int j = GetIntFromString(matches[1]);
-                    registers[j] += matches[0].StartsWith("inc") ? 1 : -1;
-                }
-                else if (matches[0].StartsWith("jnz"))
-                {
-                    int x;
-                    if (Regex.IsMatch(instructions[i], @"[a-z]"))
-                        x = registers[GetIntFromString(matches[1])];
-                    else
-                        x = int.Parse(matches[1]);
-
-                    int y = int.Parse(matches[2]);
-                    if (x != 0)
-                    {
-                        i += y;
-                    }
-
-                    Console.WriteLine("{0} {1} {2} int value {3}: \t{4}", matches[0], x, y, matches[2], i);
-                }
-                else
-                {
-                    throw new Exception();
-                }
-            }
-
-            Console.WriteLine("Value of A: {0}", registers[0]);
-            Console.Read();
+            // Part 2
+            registers[2] = 1;
+            calc();
+            Console.WriteLine("Part2: Register a = {0}", registers[0]);
+            Console.ReadLine();
         }
 
-        static private int GetIntFromString(string s)
+        static void calc()
         {
-            if (Regex.IsMatch(s, @"[0-9]+"))
+            int i = 0;
+
+            while (true)
             {
-                return int.Parse(s);
-            }
-            else
-            {
-                switch (s)
+                if (i >= instructions.Count) break;
+
+                //Console.WriteLine(" A: {0} \n B: {1} \n C: {2} \n D: {3} \n", registers[0], registers[1], registers[2], registers[3]);
+
+                string[] arr = instructions[i].Split();
+                string cmd = arr[0];
+
+                if (cmd == "cpy")
                 {
-                    case "a":
-                        return 0;
-                    case "b":
-                        return 1;
-                    case "c":
-                        return 2;
-                    case "d":
-                        return 3;
+                    int regnum = returnRegister(arr[2].ToCharArray()[0]);
+
+                    if (isNumberic(arr[1]))
+                        registers[regnum] = Convert.ToInt32(arr[1]);
+                    else
+                        registers[regnum] = registers[returnRegister(arr[1].ToCharArray()[0])];
                 }
+                else if (cmd == "inc")
+                {
+                    int regnum = returnRegister(arr[1].ToCharArray()[0]);
+                    registers[regnum]++;
+                }
+                else if (cmd == "dec")
+                {
+                    int regnum = returnRegister(arr[1].ToCharArray()[0]);
+                    registers[regnum]--;
+                }
+                else if (cmd == "jnz")
+                {
+                    int val = Convert.ToInt32(arr[2]);
+
+                    if (isNumberic(arr[1]))
+                    {
+                        if (Convert.ToInt32(arr[1]) != 0)
+                        {
+                            i += val;
+                            if (i >= instructions.Count - 1)
+                                break;
+
+                            continue;
+                        }
+                    }
+
+                    int regnum = returnRegister(arr[1].ToCharArray()[0]);
+
+                    if (regnum == -1)
+                    {
+                        i++;
+                        continue;
+                    }
+
+                    if (registers[regnum] != 0)
+                    {
+                        i += val;
+                        if (i >= instructions.Count - 1)
+                            break;
+
+                        continue;
+                    }
+                }
+
+                i++;
             }
-            throw new ArgumentOutOfRangeException();
+        }
+
+        static int returnRegister(char ch)
+        {
+            int registerValue = -1;
+
+            switch (ch)
+            {
+                case 'a':
+                    registerValue = 0;
+                    break;
+                case 'b':
+                    registerValue = 1;
+                    break;
+                case 'c':
+                    registerValue = 2;
+                    break;
+                case 'd':
+                    registerValue = 3;
+                    break;
+            }
+
+            return registerValue;
+        }
+
+        static bool isNumberic(string str)
+        {
+            int n;
+            return int.TryParse(str, out n);
         }
     }
 }
