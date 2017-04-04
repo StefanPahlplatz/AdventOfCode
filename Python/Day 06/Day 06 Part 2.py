692f3c3aeb057a2f3b7d45e672dfe5fa64ed24bf
Stefan Pahlplatz <stefan314@live.nl>
2017-04-04 12:13:17 +0200

    Python: Day 6.

---
+++ Python/Day 06/Day 06 Part 2.py
@@ -0,0 +1,19 @@
+import string
+
+# Read the file
+with open("day6.txt") as f:
+    file = f.read().split()
+
+# Alphabet as dictonary
+d = [dict.fromkeys(string.ascii_lowercase, 0) for k in range(0, len(file[0]))]
+
+# Loop through all lines
+for j in range(0, len(file)):
+    # Loop trough all characters
+    for i in range(0, len(file[0])):
+        d[i][file[j][i]] += 1
+
+# Turn the dictonary into an ordered list
+for l in range(0, len(d)):
+    ordered = sorted(d[l].items(), key=lambda x: x[1])
+    print(ordered[0][0], end="")
