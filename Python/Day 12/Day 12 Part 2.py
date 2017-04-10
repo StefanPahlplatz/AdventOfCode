9388674358ef54096998589f8162b24b0d20152d
Stefan Pahlplatz <stefan314@live.nl>
2017-04-10 20:21:57 +0200

    Python: Day 12.

---
+++ Python/Day 12/Day 12 Part 2.py
@@ -0,0 +1,38 @@
+import re
+
+registers = dict.fromkeys(['a', 'b', 'c', 'd'], 0)
+registers['c'] = 1
+
+with open("day12.txt") as f:
+    file = f.readlines()
+    i = 0
+    while True:
+        if i >= len(file):
+            print("Answer part 1:", registers['a'])
+            break
+        matches = re.findall("[^=\s]*", file[i])
+        if "cpy" in file[i]:
+            try:
+                x = int(matches[2])
+            except ValueError:
+                x = registers[matches[2]]
+            y = matches[4]
+            registers[y] = x
+
+        elif "inc" in file[i]:
+            registers[matches[2]] += 1
+
+        elif "dec" in file[i]:
+            registers[matches[2]] -= 1
+
+        else:
+            try:
+                x = int(matches[2])
+            except ValueError:
+                x = registers[matches[2]]
+
+            if x != 0:
+                i -= 1
+                y = int(matches[4])
+                i += y
+        i += 1
